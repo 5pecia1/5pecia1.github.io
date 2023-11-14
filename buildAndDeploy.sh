5ceb36277921e900d90999ea1bef27ddff2d502e
5pecia1 <pdpxpd@gmail.com>
2023-11-14 04:38:51 +0000

    add script

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -5,7 +5,7 @@ COMMIT=$(git log -1 --pretty=%B)
 git checkout master
 cp -r _site/* .
 git add .
-git commit -m $COMMIT
+git commit -m "$COMMIT"
 
 echo "git add and commit \"$COMMIT\""
 git checkout develop
